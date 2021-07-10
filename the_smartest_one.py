28ba4624b9665944d0c61eba929ab7132aea7c73
Artur Agliullin <artur_agliullin@mail.ru>
2021-07-10 19:19:07 +0500

    ver.6 Added file as part of homework(requests)

---
+++ the_smartest_one.py
@@ -0,0 +1,31 @@
+import requests
+import operator
+
+
+def search_hero_id(name: str, token=2619421814940190):
+    url = f"https://superheroapi.com/api/{token}/search/{name}"
+    response = requests.get(url)
+    resp = response.json()
+    hero_id = resp['results'][0]['id']
+    # print(f"{name}'s id is {hero_id}")
+    return hero_id
+
+
+def get_hero_intelligence(id, token=2619421814940190):
+    url = f"https://superheroapi.com/api/{token}/{id}/powerstats"
+    response = requests.get(url)
+    resp = response.json()
+    hero_int = resp['intelligence']
+    # print(f"{resp['name']}'s intelligence is {resp['intelligence']}")
+    return hero_int
+
+
+def whos_thesmartest_one(*names):
+    heroes_int = {}
+    for item in names:
+        heroes_int[item] = int(get_hero_intelligence(search_hero_id(item)))
+    sorted_tuples = sorted(heroes_int.items(), key=operator.itemgetter(1), reverse=True)
+    print(f"{sorted_tuples[0][0]} is the most intelligent hero, with {sorted_tuples[0][1]} intelligence!")
+
+
+whos_thesmartest_one('Hulk', 'Thanos', 'Captain America')
