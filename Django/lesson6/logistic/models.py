4f0cd38cbf253adb04ce01e5736778656e4ce811
Artur Agliullin <artur_agliullin@mail.ru>
2022-04-17 22:00:04 +0500

    lesson6 folder updated

--- Django/lesson6/logistic/models.py
+++ Django/lesson6/logistic/models.py
@@ -18,12 +18,3 @@ class StockProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='positions',)
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=18, decimal_places=2, validators=[MinValueValidator(0)],)
-
-
-class StockFilter(filters.FilterSet):
-    id = filters.ModelMultipleChoiceFilter(to_field_name='id', queryset=Stock.objects.all())
-    # products = filters.NumberFilter(to_field_name='products', queryset=Stock.objects.all())
-
-    class Meta:
-        model = Stock
-        fields = ['id',]
--- Django/lesson6/logistic/views.py
+++ Django/lesson6/logistic/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
-from logistic.models import Product, Stock, StockFilter
+from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
@@ -18,5 +18,5 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend, ]
-    filterset_class = StockFilter
+    filterset_fields = ['products']
     # при необходимости добавьте параметры фильтрации
