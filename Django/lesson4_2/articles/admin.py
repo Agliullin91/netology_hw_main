719cbda2a82b4df4d2248a3fb45f674a4e28b247
Artur Agliullin <artur_agliullin@mail.ru>
2022-04-20 17:45:44 +0500

    lesson4_2 folder created

---
+++ Django/lesson4_2/articles/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+
+from .models import Article, Scope, ArticleScope
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_index = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            if form.cleaned_data.get('is_main') is True:
+                main_index += 1
+        if main_index != 1:
+            raise ValidationError('Выберите ОДИН основной раздел.')
+        else:
+            return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class RelationshipInline(admin.TabularInline):
+    model = ArticleScope
+    extra = 1
+    formset = RelationshipInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'text', 'published_at']
+    inlines = [RelationshipInline, ]
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', ]
